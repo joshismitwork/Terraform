43e67cbcf295e5226ba920ea3826e972bc5bb4d1
Smit Joshi <joshismit04@gmail.com>
2026-03-14 21:56:54 -0400

    Delete s3-static-website directory

--- s3-static-website/output.tf
+++
@@ -1,4 +0,0 @@
-output "website_url" {
-  value = aws_s3_bucket.tf-s3-bucket.website_endpoint
-
-}
--- s3-static-website/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  default = "smit-tf-s3staticwebsite"
-  type    = string
-
-}
-
-variable "env" {
-  default = "dev"
-}
